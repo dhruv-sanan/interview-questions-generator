8b06a23d35a44ec2e748fb31fb72405669723feb
dhruv-sanan <dhruvsanan47@gmail.com>
2024-07-12 19:45:32 +0530

    loading data

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Gen AI Project",
+    version="0.0.0",
+    author="Dhruv Sanan",
+    author_email="dhruvsanan47@gmail.com",
+    description="A Gen AI project for the development of a chatbot for managing and analyzing research trials.",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+
+)
