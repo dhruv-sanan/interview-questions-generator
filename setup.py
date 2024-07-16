7d837375776589261d0e598801dc06dad9e47fc1
dhruv-sanan <dhruvsanan47@gmail.com>
2024-07-16 18:23:15 +0530

    email updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="Gen AI Project",
     version="0.0.0",
     author="Dhruv Sanan",
-    author_email="dhruvsanan47@gmail.com",
+    author_email="dhruvsanan2001@gmail.com",
     description="A Gen AI project for the development of a chatbot for managing and analyzing research trials.",
     packages=find_packages(),
     include_package_data=True,
