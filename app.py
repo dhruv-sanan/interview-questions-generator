0c97d17a04f767cbeee230ec7ffaccc3ffae7f00
dhruv-sanan <dhruvsanan47@gmail.com>
2024-09-13 10:39:43 +0530

    added function for file processing

---
+++ app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Form, Request, Response, File, Depends, HTTPException, status
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.encoders import jsonable_encoder
+import uvicorn
+import os
+import aiofiles
+import json
+import csv
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host='0.0.0.0', port=8080, reload=True)
